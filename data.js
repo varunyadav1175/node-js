a5032e27c9372b7da969779e50ac2b4d955af038
Varun Yadav <Varunyadav1175@gmail.com>
2024-01-03 12:33:28 +0530

    hello

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = [
     {'name':'varun', 'email':'hello@gmail.com'},
     {'name':'okay', 'email':'okay@gmail.com'},
     {'name':'halwa', 'email':'halwa@gmail.com'},
-    {'name':'puri', 'email':''}
+    {'name':'puri', 'email':'sSs'}
 ]
 
 module.exports = data;
