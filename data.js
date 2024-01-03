393d6d1b4b78c3411cdac49bf4965206dfc43a14
Varun Yadav <Varunyadav1175@gmail.com>
2024-01-03 12:14:10 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+    {'name':'varun', 'email':'hello@gmail.com'},
+    {'name':'okay', 'email':'okay@gmail.com'},
+    {'name':'halwa', 'email':'halwa@gmail.com'},
+    {'name':'puri', 'email':'puri@gmail.com'}
+]
+
+module.exports = data;
