98127c2fd2190efaf67b339a0bfb596dcdec342c
VARUN YADAV <64697068+varunyadav1175@users.noreply.github.com>
2024-01-03 12:21:55 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = [
     {'name':'varun', 'email':'hello@gmail.com'},
     {'name':'okay', 'email':'okay@gmail.com'},
     {'name':'halwa', 'email':'halwa@gmail.com'},
-    {'name':'puri', 'email':'puriii@gmail.com'}
+    {'name':'puri', 'email':'puri@gmail.com'}
 ]
 
 module.exports = data;
